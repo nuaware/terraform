dc7846280dbb81d56101f31c8400c2f6e03a0bf2
lukehas <luke@nuaware.com>
2020-01-25 09:41:05 +0000

    Adding resources

--- example/example.tf
+++ example/example.tf
@@ -7,4 +7,11 @@ resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
-
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+resource "aws_instance" "another" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
